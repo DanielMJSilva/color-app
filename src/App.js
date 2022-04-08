c55955c2832ba6bd40aa7b3b98572fec56dcf78d
DanielMJSilva <70338659+DanielMJSilva@users.noreply.github.com>
2022-04-08 16:10:38 -0400

    Add Color Helper to Generate Colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Pallete from './Pallete';
+import React, { Component } from 'react';
+import Palette from './Palette';
 import seedColors from './seedColors';
-import './App.css';
+import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div className="">
-     <Pallete {...seedColors[4]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+    return (
+      <div className="Palette">
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Palette.js
+++ src/Palette.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import './Pallete.css';
+import './Palette.css';
 
-class Pallete extends Component {
+class Palette extends Component {
   render() {
       const colorBoxes = this.props.colors.map(color => (
           <ColorBox background={color.color} name={color.name} />
-      ))
+      ));
     return (
-      <div className='Pallete'>
+      <div className='Palette'>
           {/* Navbar goes here */}
-          <div className='Pallete-colors'>
-              {colorBoxes}
-              
-          </div>
+          <div className='Palette-colors'>{colorBoxes}</div>
           {/* footer */}
       </div>
     );
   }
 }
-
-export default Pallete;
+export default Palette;
