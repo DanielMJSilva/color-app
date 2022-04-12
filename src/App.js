41941cc1f76314e1e96d94f3f0d6f0dd689c90e2
DanielMJSilva <70338659+DanielMJSilva@users.noreply.github.com>
2022-04-12 10:36:24 -0400

    Add Select and Material UI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="Palette">
-        <Palette palette={generatePalette(seedColors[4])} />
+        <Palette palette={generatePalette(seedColors[1])} />
       </div>
     );
   }
