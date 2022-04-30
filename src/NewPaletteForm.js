9f8684d066b968d391ff32782960d9cbf4dce673
DanielMJSilva <70338659+DanielMJSilva@users.noreply.github.com>
2022-04-30 06:42:36 -0400

    Add FormValidator

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -12,7 +12,8 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import { ChromePicker } from 'react-color';
 import DraggableColorBox from './DraggableColorBox';
-import { Button} from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 
 const drawerWidth = 400;
@@ -81,12 +82,25 @@ class NewPaletteForm extends Component {
         this.state = {
             open: true,
             currentColor: "teal",
-            colors: ["purple", "blue"],
+            newName: "",
+            colors: [],
         }
         this.updateCurrentColor = this.updateCurrentColor.bind(this);
         this.addNewColor = this.addNewColor.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
-
+    componentDidMount(){
+        ValidatorForm.addValidationRule('isColorNameUnique', (value) => 
+            this.state.colors.every(
+                ({name}) => name.toLowerCase() !== value.toLowerCase()
+            )
+        );
+        ValidatorForm.addValidationRule('isColorUnique', (value) => 
+            this.state.colors.every(
+                ({color}) => color !== this.state.currentColor
+            )
+        );
+    }
 
     handleDrawerOpen = () => {
         this.setState({ open: true });
@@ -99,7 +113,14 @@ class NewPaletteForm extends Component {
         this.setState({ currentColor: newColor.hex });
     }
     addNewColor() {
-        this.setState({ colors: [...this.state.colors, this.state.currentColor] });
+        const newColor = {
+            color: this.state.currentColor,
+            name: this.state.newName
+        }
+        this.setState({ colors: [...this.state.colors, newColor], newName: '' });
+    }
+    handleChange(evt) {
+        this.setState({ newName: evt.target.value });
     }
 
     render() {
@@ -153,15 +174,24 @@ class NewPaletteForm extends Component {
 
                     <ChromePicker color={this.state.currentColor} onChangeComplete={this.updateCurrentColor}
                     />
+                    <ValidatorForm onSubmit={this.addNewColor}>
+                        <TextValidator 
+                            value={this.state.newName} 
+                            name="newColor" 
+                            onChange={this.handleChange}
+                            validators={['required', 'isColorNameUnique', 'isColorUnique']}
+                            errorMessages={['Enter a color name', 'Color name must be unique', 'Color already used!']}
+                        />
+                        <Button
+                            variant="contained"
+                            type='submit'
+                            color="primary"
+                            style={{ backgroundColor: this.state.currentColor }}
+                        >
+                            ADD COLOR
+                        </Button>
+                    </ValidatorForm>
 
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        style={{ backgroundColor: this.state.currentColor }}
-                        onClick={this.addNewColor}
-                    >
-                        ADD COLOR
-                    </Button>
 
                 </Drawer>
                 <main
@@ -170,9 +200,9 @@ class NewPaletteForm extends Component {
                     })}
                 >
                     <div className={classes.drawerHeader} />
-                        {colors.map(color => (
-                        <DraggableColorBox color={color} />
-                        ))}
+                    {colors.map(color => (
+                        <DraggableColorBox color={color.color} name={color.name} />
+                    ))}
                 </main>
             </div>
         );
