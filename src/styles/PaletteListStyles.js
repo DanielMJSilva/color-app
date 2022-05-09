b7d72103514bfe463874260b1540e6794eb5d6a1
DanielMJSilva <70338659+DanielMJSilva@users.noreply.github.com>
2022-05-08 23:46:20 -0400

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+import sizes from "./sizes";
+
+const styles = {
     root: {
         backgroundColor: "blue",
         height: "100vh",
@@ -12,7 +13,13 @@ export default {
         display: "flex",
         alignItems: "flex-start",
         flexDirection: "column",
-        flexWrap: "wrap"
+        flexWrap: "wrap",
+        [sizes.down("xl")]: {
+            width: "80%"
+        },
+        [sizes.down("sm")]: {
+            width: "75%"
+        }
     },
     nav: {
         display: "flex",
@@ -29,6 +36,15 @@ export default {
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%"
+        gridGap: "2.5rem",
+        [sizes.down("md")]: {
+            gridTemplateColumns: "repeat(2, 50%)",
+        },
+        [sizes.down("xs")]: {
+            gridTemplateColumns: "repeat(1, 100%)",
+            gridGap: "1rem",
+        }
     }
 };
+
+export default styles;
